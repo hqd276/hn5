831e02e1fea8e10b2bd2fe91cd4da0e456f9dba7
Hoang Dung <dunghq87@gmail.com>
2016-05-30 07:36:33 +0700

    V 0.44

--- application/config/database.php
+++ application/config/database.php
@@ -50,11 +50,11 @@ $active_record = TRUE;
 
 // $db['default']['hostname'] = 'localhost';
 // $db['default']['username'] = 'root';
-// $db['default']['password'] = 'evui@1983';
+// $db['default']['password'] = '123654789';
 // $db['default']['database'] = 'hn5';
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'evui@1983';
+$db['default']['password'] = '123654789';
 $db['default']['database'] = 'hn5';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
